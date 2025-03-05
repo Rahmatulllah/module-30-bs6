1edf99b4641f65382a0ab4dee6c78fd024f64a94
Rahmatullah <hrmilon1997@gmail.com>
2025-03-05 10:47:26 +0600

    second commit

---
+++ Js/es6.js
@@ -0,0 +1,14 @@
+
+
+
+// const friends = ["hena","hero alom"];
+
+// friends.push("Jannat")
+ 
+// for (let i = 0; i < friends.length; i++) {
+//     const element = friends[i];
+
+//     console.log(element);
+// }
+
+
