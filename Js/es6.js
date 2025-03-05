19852efdeb0ab30f5a87ef5f27fe17fed4af0e6b
Rahmatullah <hrmilon1997@gmail.com>
2025-03-05 16:02:35 +0600

    added string template,default paramitres

--- Js/es6.js
+++ Js/es6.js
@@ -11,4 +11,13 @@
 //     console.log(element);
 // }
 
+// default paramiter num1=0 num2=0 equation wise set korte hobe
+
+// string template
+// const template = `amr shonar bangla ami tomay valobashi ami jani tmi ashbe akhane ami joto dur jani tmi amk chara thakte parbe na`;
+// console.log(template);
+
+// template string er vitore dinamic kono variable likhte hole ${variable} avabe likht hobe
+
+
 
